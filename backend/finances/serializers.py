cfe73d51d894e02a47e2d52946067673247ee6d2
Miguel Henrique <miguelhenriquemelo17@gmail.com>
2026-06-08 15:28:36 -0300

    feat: serializers.py

---
+++ backend/finances/serializers.py
@@ -0,0 +1,28 @@
+from rest_framework import serializers
+from .models import Category, Transaction, Budget, RecurringExpense
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        field = ['id', 'name', 'icon']
+
+class TransactionSerializer(serializers.ModelSerializer):
+    category_name = serializers.CharField(source='category.name', read_only=True)
+
+    class Meta:
+        model = Transaction
+        fields = ['id', 'category', 'category_name', 'amount', 'month', 'year']
+
+class BudgetSerializer(serializers.ModelSerializer):
+    category_name = serializers.CharField(source='category.name', read_only=True)
+
+    class Meta:
+        model = Budget
+        fields = ['id', 'category', 'category_name', 'amount', 'month', 'year']
+
+class RecurringExpenseSerializer(serializers.ModelSerializer):
+    category_name = serializers.Charfield(soruce='category.name', read_only=True)
+
+    class Meta:
+        model = RecurringExpense
+        fields = ['id', 'category', 'category_name', 'description', 'amount', 'frequency', 'start_date', 'is_activate']
